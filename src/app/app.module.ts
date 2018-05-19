230514d31fdc21cbb251a6755a89a209cd7a109c
Alexey Miroshnychenko <muzz0neasdqwe@gmail.com>
2018-05-19 13:13:33 +0300

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
+import { PhoneBookComponent } from './phone-book/phone-book.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneBookComponent,
+    AddContactComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    MatToolbarModule,
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
